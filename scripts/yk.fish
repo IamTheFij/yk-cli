c688f8ce00149244519519e6fdcd61a2a1dac4ce
Ian Fijolek <ian@iamthefij.com>
2021-07-22 12:01:26 -0700

    Add support for a couple other clipboard tools to helper script

--- scripts/yk.fish
+++ scripts/yk.fish
@@ -6,5 +6,15 @@ complete -c yk -a set-password -d "Set authentication password"
 complete -c yk -a "(yk list)" -d "Credential to get TOTP for"
 
 function yk-copy --description "Select a credential using fzf and copy it to clipboard"
-    yk (yk list | fzf) | pbcopy
+    set result (yk (yk list | fzf))
+
+    if command -q pbcopy
+        echo "$result" | pbcopy
+    else if command -q xsel
+        echo "$result" | xsel --clipboard
+    else if command -q xclip
+        echo "$result" | xclip
+    else
+        echo "No clipboard command found. Code is $result"
+    end
 end
